c8576d44b30302627d801e8b6456fd8b88ce3bb7
Andrew Millman <andrewmillman35@gmail.com>
2015-08-08 02:33:56 -0700

    Add project file

---
+++ Wishio/Wishio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wishio
+//
+//  Created by Andrew on 8/8/15.
+//  Copyright (c) 2015 Wishio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
